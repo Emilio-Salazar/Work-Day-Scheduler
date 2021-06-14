65436bac10ebabc24ee7e739a7c4f8fa6ee27445
E Lazar <emiliosalazar4@gmail.com>
2021-06-13 21:07:54 -0500

    basic structure of code added

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,5 +1,21 @@
 
 var currentDate = moment().format("MMMM Do YYYY");
 console.log(currentDate);
+console.log(moment().format("H"));
+console.log($(".time-block #6").attr("id"))
 $("#currentDay").append(currentDate);
-currentTime = moment().format()
+currentTime = moment().format("H")
+
+currentTimeColor = function() {
+ 
+    for (let i =5; i<19; i++) {
+        var getID = $(".time-block #i").attr("id")
+        debugger;
+        if (moment(getID).isBefore(currentTime)) {
+            $("#i").addClass("past")
+            
+        }
+    }
+
+}
+currentTimeColor();
